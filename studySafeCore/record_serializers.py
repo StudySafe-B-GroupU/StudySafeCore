e433737f602d9ba5b199531d1b054b5a6848db5f
sowahei <ivan.so@mykpi.com>
2022-05-03 14:06:34 +0800

    bug fix

--- studySafeCore/record_serializers.py
+++ studySafeCore/record_serializers.py
@@ -5,5 +5,4 @@ class RecordsSerializer(serializers.Serializer):
     id  =serializers.IntegerField(read_only =True)
     hku_id = serializers.CharField(max_length = 10)
     venueCode = serializers.CharField(max_length = 200)
-    date = serializers.DateField()
-    time = serializers.TimeField()
+    date_time = serializers.DateTimeField()
