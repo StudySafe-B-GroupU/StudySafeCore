c46036b7352c90f46c37f0fcced65bf983e77744
sowahei <ivan.so@mykpi.com>
2022-05-06 03:53:02 +0800

    bug fix

--- demo/settings.py
+++ demo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str('QDD_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG= False #env.bool('QDD_DEBUG', default=False)
+DEBUG= env.bool('QDD_DEBUG', default=False)
 
 ALLOWED_HOSTS = ['blooming-badlands-56728.herokuapp.com', 'localhost','127.0.0.1']
 
